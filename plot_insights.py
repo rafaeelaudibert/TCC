2397b4bfbc146b5b9f34c93856a161503247468e
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-07-04 15:49:43 -0300

    :sparkles: Added features to the plot file

--- plot_insights.py
+++ plot_insights.py
@@ -2,47 +2,94 @@ import json
 import marshal
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn import linear_model
+from scipy import stats
+import numpy as np
 import matplotlib.pyplot as plt
 import fire
 import pprint
+from ordered_set import OrderedSet
 
 CONFERENCES_BASE_PATH = './conferences/'
 AUTHORS_BASE_PATH = './turing_awards/'
+IMAGE_FOLDER_PATH = './img/'
 
-def fetch_title(entry):
+def fetch_title(entry: dict):
     return entry['title'] if type(entry['title']) is str else entry['title']['#text']
 
-def main(conferences_input_file = 'dblp.json', author_input_file = 'author_dblp.json'):
+def main(conferences_input_file: str = 'dblp.json', author_input_files: str = 'author_dblp.json', plot_title: str = 'Title', award_year: int = None):
     
-    # Read conferences file
+    # Read conferences file, and parse their corpus by year and in its entirety
     print('[INFO] Fetching conferences corpus')
+    conferences_corpus_by_year = {}
     with open(CONFERENCES_BASE_PATH + conferences_input_file, 'r') as f:
-        conferences_corpus = [fetch_title(entry) for entry in json.load(f)]
+        full_data: list = json.load(f)
+        for entry in full_data:
+            if entry['year'] not in conferences_corpus_by_year:
+                conferences_corpus_by_year[entry['year']] = {'corpus': []}
+            
+            conferences_corpus_by_year[entry['year']]['corpus'].append(fetch_title(entry))
 
     # Read author file
-    print('[INFO] Fetching author corpus')
-    with open(AUTHORS_BASE_PATH + author_input_file, 'r') as f:
-        author_corpus = [fetch_title(entry) for entry in json.load(f)]
+    print('[INFO] Fetching author(s) corpus')
+    author_corpus = []
+    for author_path in author_input_files.split(','):
+        with open(AUTHORS_BASE_PATH + author_path, 'r') as f:
+            author_corpus += [fetch_title(entry) for entry in json.load(f)]
 
-    # Compute full corpus
-    print('[INFO] Computing full corpus')
-    full_corpus = conferences_corpus + author_corpus
+   
+    # Run TF-IDF, averaging it for each feature for the conference years
+    print('[INFO] Computing TF-IDF for the conferences corpus')
+    for year in conferences_corpus_by_year.keys():
+        vectorizer = TfidfVectorizer(stop_words='english')
+        conferences_corpus_by_year[year]['tf-idf'] = vectorizer.fit_transform(conferences_corpus_by_year[year]['corpus'])
+        conferences_corpus_by_year[year]['mean-tf-idf'] = sorted(list(zip(conferences_corpus_by_year[year]['tf-idf'].mean(0).transpose(), vectorizer.get_feature_names())), key=lambda x: x[0], reverse=True)
 
-    # Run TF-IDF
-    print('[INFO] Computing TF-IDF for the full corpus')
+    # Run TF-IDF, averaging it for each feature for the author papers
+    print('[INFO] Computing TF-IDF for the author corpus')
     vectorizer = TfidfVectorizer(stop_words='english')
-    X = vectorizer.fit_transform(full_corpus)
-    pprint.pprint(X)
+    author_corpus_dict = {}
+    author_corpus_dict['tf-idf'] = vectorizer.fit_transform(author_corpus)
+    author_corpus_dict['mean-tf-idf'] = sorted(list(zip(author_corpus_dict['tf-idf'].mean(0).transpose(), vectorizer.get_feature_names())), key=lambda x: x[0], reverse=True)
+    
+    # Calculating the Spearman's correlation for each year
+    print('[INFO] Computing Spearman\'s correlation for each year conference corpus')
+    plot_data = {}
+    for year in sorted(conferences_corpus_by_year.keys()):
+        conference_ranking = [entry[1] for entry in conferences_corpus_by_year[year]['mean-tf-idf']]
+        author_ranking = [entry[1] for entry in author_corpus_dict['mean-tf-idf']]
 
-    # Run Singular Value Decomposition
-    print('[INFO] Computing SVD in the TF-IDF')
-    svd = TruncatedSVD()
-    svd.fit(X)
-    pprint.pprint(svd.components_)
+        conference_ranking_set = OrderedSet(conference_ranking)
+        author_ranking_set = OrderedSet(author_ranking)
+        intersected_set = author_ranking_set.intersection(conference_ranking_set)
+
+        intersected_conference_ranking = [conference_ranking_set.index(intersection) for intersection in intersected_set]
+        intersected_author_ranking = [author_ranking_set.index(intersection) for intersection in intersected_set]
+    
+        sc, p = stats.spearmanr(intersected_author_ranking, intersected_conference_ranking)
+        plot_data[year] = sc
+    
+    pprint.pprint(plot_data)
+
+
+    # Create regression object + computing it
+    print("[INFO] Computing linear regression")
+    X = np.array([int(x) for x in plot_data.keys()]).reshape(-1, 1)
+    Y = list(plot_data.values())
+    regr = linear_model.LinearRegression()
+    regr.fit(X, Y)
+    Y_pred = regr.predict(X)
 
     # Plotting scattered data
-    plt.scatter(*svd.components_)
-    plt.show()
+    print("[INFO] Plotting data")
+    plt.plot(X, Y_pred, color='blue', linewidth=3)
+    if award_year is not None:
+        plt.scatter([award_year], [plot_data[str(award_year)]], c='orange', marker='*', s=500)
+    plt.scatter(X, Y, color='green')
+    plt.title(plot_title)
+    plt.savefig(IMAGE_FOLDER_PATH + plot_title + '.png')
+    # plt.show()
 
 
 
