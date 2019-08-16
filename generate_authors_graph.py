2f6f284166781f92e67fc8dda429a02fac2bbe54
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-08-16 12:42:38 -0300

    :sparkles: Updated parameters from Fire and code execution

--- generate_authors_graph.py
+++ generate_authors_graph.py
@@ -40,52 +40,84 @@ def get_data(dictionary):
         'references': dictionary.get('references', []),
     }
 
-def main(save_adj: bool = False, save_conf: bool = False, figure_save_path: str = "graph.svg", save_gml: bool = False, plot_to_files: bool = False):
+
+def generate_graph(save_records: bool = False,
+                   save_gml: bool = False,
+                   plot_graph_figure: bool = False,
+                   read_from_dblp: bool = False,
+                   generate_graph: bool = False,
+                   figure_save_path: str = "graph.svg",
+                   graph_file: str = 'graph.gml',
+                   dblp_filename: str = 'dblp_papers_v11.txt'):
     G = nx.DiGraph()
     conference_papers = {}
 
     # Read file adding to array
+    if read_from_dblp:
+        with open('./dblp_arnet/{}'.format(dblp_filename), 'r') as f:
             for line in tqdm(f, total=DATASET_SIZE):
             parsed_json = json.loads(line)
             try:
                     if parsed_json['venue']['id'] in CONFERENCE_IDS:
                     # If doesn't have year in the dictionary
                     if parsed_json['year'] not in conference_papers:
-                        conference_papers[int(parsed_json['year'])] = []
+                            conference_papers[parsed_json['year']] = []
 
-                    conference_papers[int(parsed_json['year'])].append(get_data(parsed_json))
+                        conference_papers[parsed_json['year']].append(
+                            get_data(parsed_json))
             except KeyError as e:
                 pass
 
     older_papers = {}
-    for year in sorted(conference_papers.keys()):
 
-        # Add papers and authors to be referenced
-        for paper in conference_papers[year]:
-            older_papers[paper['id']] = [author for author in paper['authors']]
+    for year in range(1890, 2020):
 
-        print("I'm in year {}".format(str(year)))
+        if generate_graph and read_from_dblp:
+            print("Parsing year {}".format(str(year)))
 
-        for paper in conference_papers[year]:
+            # Add papers and authors to be referenced later
+            for paper in conference_papers.get(year, []):
+                older_papers[paper['id']] = [
+                    author for author in paper['authors']]
             
+            for paper in conference_papers.get(year, []):
             # Adiciona/atualiza nodos dos autores
             for author in paper['authors']:          
-                G.add_node(author['id'], name=author['name'] if 'name' in author else '')
+                    G.add_node(author['id'],
+                               name=author.get('name', ''),
+                               count=G.node[author['id']].get('count', 0) + 1 if author['id'] in G.nodes() else 1)  # nopep8
             
             # Adiciona arestas
             for citation_id in paper['references']:
                 if citation_id in older_papers:
-                    for other_author in older_papers[citation_id]:  # Other paper authors
-                        for author in paper['authors']:             # This paper authors
+                        # Other paper authors
+                        for other_author in older_papers[citation_id]:
+                            # This paper authors
+                            for author in paper['authors']:
                             G.add_edge(author['id'], other_author['id'])
-        print("Current Graph size: {} nodes and {} edges".format(G.number_of_nodes(), G.number_of_edges()))
 
-        if plot_to_files:
+            print("Current Graph size: {} nodes and {} edges".format(
+                G.number_of_nodes(), G.number_of_edges()))
+        else:
+            gml_filename = DATASET_BASE_PATH + \
+                '{}_{}_{}'.format(CONFERENCE_NAME, year, graph_file)
+            print("Reading graph from GML file {}".format(gml_filename))
+            try:
+                G = nx.read_gml(gml_filename)
+                print("Finished reading graph from GML file")
+            except FileNotFoundError:
+                G = nx.DiGraph()
+                print("Generating empty graph, as there is no file")
+
+        if plot_graph_figure:
             # Draw graph
             f = plt.figure()
             plt.title("Authors references graph until {}".format(str(year)))
             node_sizes = [degree + 1 for node, degree in list(G.in_degree())]
-            nx.draw_spring(G, node_size=node_sizes, alpha=0.4, arrowsize=2, arrowstyle='- >')
+            nx.draw_spring(G, node_size=node_sizes, alpha=0.4,
+                           arrowsize=2, arrowstyle='- >')
+
+            # Save graph
             f.savefig(str(year) + "_authors_" + figure_save_path)
             # plt.show()
             plt.close()
@@ -106,15 +138,18 @@ def main(save_adj: bool = False, save_conf: bool = False, figure_save_path: str 
             json.dump(data, f)
         print("Saved adjacency networkx matrix")
 
+    # Save graph to .gml
     if save_gml:    
-        nx.write_gml(G, './dblp_arnet/{}_graph.gml'.format(CONFERENCE_NAME))
+        nx.write_gml(G, DATASET_BASE_PATH +
+                     '{}_{}'.format(CONFERENCE_NAME, graph_file))
         print("Saved graph to .gml file")
 
     # Write json data to file
-    if save_conf:
+    if save_records:
         with open('./dblp_arnet/{}.json'.format(CONFERENCE_NAME), 'w') as f:
             json.dump(conference_papers, f)
         print("Saved conference json")
 
+
 if __name__ == "__main__":
-    fire.Fire(main)
+    fire.Fire(generate_graph)
