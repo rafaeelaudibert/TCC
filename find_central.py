009d075e3f8019144c7e6aa095281dedc1c09ad0
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-10-31 16:03:14 -0300

    :recycle: Made find_central more generic
    Now we can pass a glob path instead of a single file

--- find_central.py
+++ find_central.py
@@ -1,37 +1,47 @@
 # Core imports
 import json
 import os
+from glob import glob
 
 # Library imports
 import networkx as nx
 import fire
+from tqdm import tqdm
 
 SAVE_FOLDER = './sorted_data/'
 
 
 def sort_and_save(data, filename: str, key: str):
     write_filename = SAVE_FOLDER + \
-        filename.split('/')[0] + ".{}.json".format(key)
+        filename.split('/')[-1] + ".{}.json".format(key)
 
     with open(write_filename, 'w') as f:
-        print('Sorting by {}'.format(key))
-        sorted_data = sorted(data, key=lambda id, data: data[key])
-        print('Saving it')
+        tqdm.write('Sorting by {}'.format(key))
+        sorted_data = sorted(
+            data, key=lambda data: data[1].get(key, ''), reverse=True)
+        tqdm.write('Saving it')
         json.dump(sorted_data, f)
-        print('Saved!')
+        tqdm.write('Done!')
 
 
-def find_central(filename: str):
-    G = nx.read_gml(filename)
+def find_central(search_path: str):
+    """
+        path: str = Name of a GML file which we want to sort
+            by their features or a path where we want to do it
+            in all the files contained by it
+    """
 
-    data = list(G.nodes(data=True))
+    for filename in tqdm(glob(search_path)):
 
-    sort_and_save(data, filename, 'degree')
-    sort_and_save(data, filename, 'indegree')
-    sort_and_save(data, filename, 'outdegree')
-    sort_and_save(data, filename, 'closeness')
-    sort_and_save(data, filename, 'betweenness')
-    sort_and_save(data, filename, 'pagerank')
+        tqdm.write("Reading {}".format(filename))
+        data = list(nx.read_gml(filename).nodes(data=True))
+
+        sort_and_save(data, filename, 'degree')
+        sort_and_save(data, filename, 'indegree')
+        sort_and_save(data, filename, 'outdegree')
+        sort_and_save(data, filename, 'closeness')
+        sort_and_save(data, filename, 'betweenness')
+        sort_and_save(data, filename, 'pagerank')
 
 
 if __name__ == "__main__":
