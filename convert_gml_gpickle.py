a511f3fbb9aa36c85333f3834331a2e420361ed9
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-12-11 17:09:26 -0300

    :recycle: Add fire and remove function to convert_gml_gpickle

--- convert_gml_gpickle.py
+++ convert_gml_gpickle.py
@@ -1,8 +1,21 @@
 import networkx as nx
 from glob import glob
 from tqdm import tqdm
+import fire
+import os
 
-for filename in tqdm(glob('./GML/*.gml')):
-    tqdm.write('Parsing {}'.format(filename))
-    G = nx.read_gml(filename)
-    nx.write_gpickle(G, filename.replace('gml', 'gpickle'))
+
+def convert(path: str, remove: bool = False):
+    for filename in tqdm(glob(path)):
+        if '.gml' in filename:
+            tqdm.write('Parsing {}'.format(filename))
+            G = nx.read_gml(filename)
+            nx.write_gpickle(G, filename.replace('gml', 'gpickle'))
+
+            # Remove the original .gml
+            if remove:
+                os.remove(filename)
+
+
+if __name__ == "__main__":
+    fire.Fire(convert)
