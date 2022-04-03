0d35c75022977b2ff3773450813a80c86d96656f
Rafael <rafaeelaudibert@gmail.com>
2022-04-03 02:38:22 -0300

    Plot some charts for countries existence

--- graph_generation/generate_country_citation_graph.py
+++ graph_generation/generate_country_citation_graph.py
@@ -1,5 +1,7 @@
 # Main class import
-from collections import defaultdict
+from collections import Counter, defaultdict
+from typing import List
+import numpy as np
 
 from tqdm import tqdm, trange
 from generate_graph import GenerateGraph
@@ -12,6 +14,23 @@ from pprint import pprint as pp
 # Library imports
 import networkx as nx
 import fire
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## COMMAND USED TO RUN KINDA EVERYTHING
+# python3 graph_generation/generate_graph.py --run-country-citation-graph --conference_name="AAAI-NIPS-IJCAI" --generate-graph --save-non-cummulated-yearly-gpickle --save-yearly-gpickle --save-non-cummulated-yearly-gpickle --save-gpickle
+
+
+# Global graph configurations
+sns.set_style("white")
+plt.rc("axes", titlesize=18)  # fontsize of the axes title
+plt.rc("axes", labelsize=14)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=13)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=13)  # fontsize of the tick labels
+plt.rc("legend", fontsize=13)  # legend fontsize
+plt.rc("font", size=13)  # controls default text sizes
+
+colors = sns.color_palette("husl", 16)
 
 # Constants
 CONFERENCE_IDS = ["1184914352", "1127325140", "1203999783"]
@@ -21,33 +40,33 @@ GRAPH_TYPE = "countries_citation"
 COUNTRY_REPLACEMENT = {}
 
 
-def infer_country_from(organization: str):
-    # Parse last name
-    country = organization.split(", ")[-1].replace(".", "").lower()
+def infer_country_from(organizations: List[str]):
+    for organization in organizations:
+        # Parse last name
+        country = organization.split(", ")[-1].replace(".", "").lower()
 
-    # Remove those pesky tabs
-    country = country.replace("#TAB#", "")
-    country = country.replace("#tab#", "")
+        # Remove those pesky tabs
+        country = country.replace("#TAB#", "")
+        country = country.replace("#tab#", "")
 
-    # Remove useless characters
-    for old, new in [("(", ""), (")", ""), ("[", ""), ("]", ""), ("-", " "), ("_", " ")]:
-        country = country.replace(old, new)
+        # Remove useless characters
+        for old, new in [("(", ""), (")", ""), ("[", ""), ("]", ""), ("-", " "), ("_", " ")]:
+            country = country.replace(old, new)
 
-    # Make country replacement
-    if country in COUNTRY_REPLACEMENT.keys():
-        return (True, COUNTRY_REPLACEMENT[country])
+        # Make country replacement
+        if country in COUNTRY_REPLACEMENT.keys():
+            return (True, COUNTRY_REPLACEMENT[country])
 
-    # Only last name
-    last_name = country.split(" ")[-1]
-    if last_name in COUNTRY_REPLACEMENT.keys():
-        return (True, COUNTRY_REPLACEMENT[last_name])
+        # Only last name
+        last_name = country.split(" ")[-1]
+        if last_name in COUNTRY_REPLACEMENT.keys():
+            return (True, COUNTRY_REPLACEMENT[last_name])
 
-    # Only first name
-    first_name = country.split(" ")[0]
-    if first_name in COUNTRY_REPLACEMENT.keys():
-        return (True, COUNTRY_REPLACEMENT[first_name])
+        # Only first name
+        first_name = country.split(" ")[0]
+        if first_name in COUNTRY_REPLACEMENT.keys():
+            return (True, COUNTRY_REPLACEMENT[first_name])
 
-    print(first_name, last_name, country)
     return (False, country)
 
 
@@ -72,6 +91,7 @@ class CountryCitationGraph(GenerateGraph):
         read_from_dblp: bool = False,
         save_from_dblp: bool = False,
         read_saved_from_dblp: bool = False,
+        generate_institutions_countries_count_chart: bool = False,
         generate_graph: bool = False,
         compute_degree: bool = False,
         compute_closeness: bool = False,
@@ -99,6 +119,9 @@ class CountryCitationGraph(GenerateGraph):
 
             generate_graph (bool): Generates the authors_and_papers
                 graph from the  articles read
+
+            generate_institutions_countries_count_chart(bool): Generates a chart showing
+            the count of institutions per countyr
         """
 
         self.G = nx.MultiDiGraph()
@@ -116,43 +139,58 @@ class CountryCitationGraph(GenerateGraph):
         # Store missing countries
         missing_set = set()
 
+        # Used countries
+        used_countries_set = set()
+
         # Store papers count per conference
         papers_count = defaultdict(lambda: 0)
 
         # Store papers count per year
         papers_year_count = defaultdict(lambda: 0)
 
+        # Store how many papers we had per country per year
+        papers_per_year_per_country = defaultdict(lambda: defaultdict(lambda: 0))
+
         # Iterate through all the dataset years
-        for year in trange(self.min_year, self.max_year):
+        for year in trange(self.min_year, self.max_year, desc="Years"):
+
+            year_papers = conference_papers.get(str(year), [])
 
             # Reset yearly graph
             if save_non_cummulated_yearly_gpickle:
                 self.yearly_G = nx.DiGraph()
 
-            if generate_missing_countries:
-                print(f"Generating missing countries for year {str(year)}")
+            tqdm.write(f"Detecting countries for year {str(year)}")
 
-                for paper in tqdm(conference_papers.get(str(year), [])):
-                    for author in paper["authors"]:
-                        for org in [author.get("org", ""), *author.get("orgs", [])]:
-                            found, country = infer_country_from(org)
-                            if not found and country not in missing_set:
-                                missing_set.add(country)
+            for paper in tqdm(year_papers, desc="Parsing Countries"):
+                for author in paper["authors"]:
+                    author_country = None
+                    for org in [author.get("org", ""), *author.get("orgs", [])]:
+                        found, country = infer_country_from([org])
 
-                    papers_count[paper["venue"]["raw"]] += 1
-                    papers_year_count[year] += 1
+                        if found and author_country is None:
+                            author_country = country
 
-            elif generate_graph and read_from_dblp:
-                print(f"Parsing year {str(year)}")
+                        if not found and country not in missing_set:
+                            missing_set.add(country)
+                        else:
+                            used_countries_set.add(country)
+
+                papers_per_year_per_country[year][author_country] += 1
+                papers_count[paper["venue"]["raw"]] += 1
+                papers_year_count[year] += 1
+
+            if generate_graph:
+                tqdm.write(f"Parsing year {str(year)}")
 
                 # Add papers and authors to be referenced later
-                for paper in conference_papers.get(year, []):
+                for paper in year_papers:
                     older_papers[paper["id"]] = [author for author in paper["authors"]]
 
-                for paper in conference_papers.get(year, []):
+                for paper in tqdm(year_papers, desc="Conference Papers"):
                     # Adiciona/atualiza nodos dos países
                     for author in paper["authors"]:
-                        country = infer_country_from(author.get("org", ""))
+                        country = infer_country_from([author.get("org", ""), *author.get("orgs", [])])
                         if country is not None:
                             self.G.add_node(country)
 
@@ -164,21 +202,27 @@ class CountryCitationGraph(GenerateGraph):
                         # Other paper authors
                         if citation_id in older_papers:
                             for other_author in older_papers[citation_id]:
+                                other_country = infer_country_from(
+                                    [other_author.get("org", ""), *other_author.get("orgs", [])]
+                                )
                                 for author in paper["authors"]:
                                     # Fetch countries
-                                    country = infer_country_from(author.get("org", ""))
-                                    other_country = infer_country_from(other_author.get("org", ""))
+                                    country = infer_country_from([author.get("org", ""), *author.get("orgs", [])])
 
                                     # Add edge
                                     if country is not None and other_country is not None:
                                         self.G.add_edge(country, other_country)
 
                                         if save_non_cummulated_yearly_gpickle:
+                                            import pdb
+
+                                            pdb.set_trace()
+
                                             self.yearly_G.add_edge(country, other_country)
 
                 self.print_graph_info()
-            else:
-                self.G = self.read_from_gpickle(year)
+            # else:
+            # self.G = self.read_from_gpickle(year)
 
             # Saving graph to .gpickle file
             if save_yearly_gpickle and self.G.number_of_nodes() > 0:
@@ -204,16 +248,78 @@ class CountryCitationGraph(GenerateGraph):
 
                     super().save_yearly_gpickle(year, G=self.yearly_G, graph_name="yearly_" + self.graph_name)
 
-        with open("../data/missing_countries.json", "w") as f:
-            json.dump({country: "" for country in missing_set}, f)
+        print(papers_per_year_per_country.values())
 
-        count = sum(len(value) for value in conference_papers.values())
-        print("Papers count per conference", papers_count)
-        print("Papers count per year", papers_year_count)
-        print("Total papers count: ", count)
+        # Just use the COUNTRY_REPLACEMENT array to generate a quick chart
+        if generate_institutions_countries_count_chart:
+            values = [
+                "-" if value is None else value
+                for value in list(COUNTRY_REPLACEMENT.values())
+                if value in used_countries_set
+            ]
+            counter = Counter(values)
 
-        if generate_graph and read_from_dblp:
-            print("Finished creating the graph")
+            most_common = counter.most_common()
+            labels, values = [key for key, val in most_common], [val for key, val in most_common]
+            print(labels)
+
+            # First figure is a bar chart with how many institutions every used country has
+            plt.xticks(rotation="vertical")
+            fig = plt.figure(figsize=(13, 16), tight_layout=True)
+            ax = fig.add_subplot(111)
+
+            bars = ax.barh(labels[::-1], values[::-1], color=colors[:5])
+            ax.bar_label(bars)
+
+            ax.set_xlabel("Organizations Count")
+            ax.set_ylabel("Countries")
+            ax.set_title("Organizations Count x Country")
+            fig.savefig("country_replacement_bar.png")
+
+            # Second figure is a stacked country chart showing how many publications
+            # per country we have every year
+            years = list(papers_per_year_per_country.keys())
+
+            last_year = years[-1]
+            last_year_papers = papers_per_year_per_country[last_year]
+            top_countries_labels = [
+                label for label, _value in sorted(last_year_papers.items(), key=lambda x: -x[1]) if label is not None
+            ][:15]
+            values = np.array(
+                [
+                    [
+                        year_values.get(country if country != "None" else "-", 0)
+                        for year_values in papers_per_year_per_country.values()
+                    ]
+                    for country in top_countries_labels
+                ]
+            )
+            percent = values / values.sum(axis=0).astype(float) * 100
+
+            fig = plt.figure(figsize=(15, 15), tight_layout=True)
+            ax = fig.add_subplot(111)
+
+            ax.stackplot(years, percent, colors=colors, labels=top_countries_labels)
+            ax.set_xticks(range(1970, 2015 + 1, 5))
+            ax.set_title("Stacked percentage per country per year")
+            ax.set_ylabel("Percent (%)")
+            ax.margins(0, 0)  # Set margins to avoid "whitespace"
+
+            # Shrink current axis's on the bottom
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0 + box.height * 0.001, box.width, box.height * 0.999])
+
+            # Put a legend below current axis
+            ax.legend(loc="upper center", bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
+
+            fig.savefig("countries_stacked.png")
+
+        if generate_missing_countries:
+            with open("../data/missing_countries.json", "w") as f:
+                json.dump({country: "" for country in missing_set}, f)
+
+        if generate_graph:
+            tqdm.write("Finished creating the graph")
             self.print_graph_info()
 
         # Save graph to .gpickle
